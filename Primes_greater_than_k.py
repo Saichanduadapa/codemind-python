7009ea9e2ba972acbc79dc33a5acbe6e44ab384b
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-07-20 19:32:03 +0530

    import math n=int(input()) lst=list(map(int,input().split())) k=int(input()) c=0 #print(lst,k) for i in lst:     if i==1:         continue     for j in range(2,int(math.sqrt(i)+1)):         if i%j==0:             break     else:         if i>=k:             c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,16 @@
+import math
+n=int(input())
+lst=list(map(int,input().split()))
+k=int(input())
+c=0
+#print(lst,k)
+for i in lst:
+    if i==1:
+        continue
+    for j in range(2,int(math.sqrt(i)+1)):
+        if i%j==0:
+            break
+    else:
+        if i>=k:
+            c+=1
+print(c)

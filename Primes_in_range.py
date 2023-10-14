cece1b74c0c26cba6028d59ab2f1ab41dc4f8b77
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-10-14 21:20:55 +0530

    import math def is_prime(n):     if(n<2):         return False     else:         for i  in range(2,int(math.sqrt(n))+1):             if(n%i==0):                 return False         return True n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if(is_prime(i)):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,17 +1,16 @@
 import math
-a=int(input())
-b=int(input())
-c,flag=0,0
-for i in range(a,b+1):
-    if i==1:
-        continue
-    flag=0
-    for j in range(2,int(math.sqrt(i))+1):
-        if i%j==0:
-            flag=1
-            break
-    if flag==0:
+def is_prime(n):
+    if(n<2):
+        return False
+    else:
+        for i  in range(2,int(math.sqrt(n))+1):
+            if(n%i==0):
+                return False
+        return True
+n=int(input())
+m=int(input())
+c=0
+for i in range(n,m+1):
+    if(is_prime(i)):
         c+=1
 print(c)
-    
-            

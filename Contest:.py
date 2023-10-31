ad1873c85e36cb56830285b668ff5076d3789f29
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-10-31 20:42:31 +0530

    a,b,c=map(int,input().split()) if b+c*2>=a:     print("Qualify") else:     print("Not Qualify")

--- Contest:.py
+++ Contest:.py
@@ -1,7 +1,5 @@
-def contest(a,b,c):
-    if b*1+c*2>=a:
-        return("Qualify")
-    else:
-        return("Not Qualify")
 a,b,c=map(int,input().split())
-print(contest(a,b,c))
+if b+c*2>=a:
+    print("Qualify")
+else:
+    print("Not Qualify")

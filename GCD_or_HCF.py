d3ca6d949f8b5acd2e7d349acda1f96804ae36f3
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-12-28 19:04:14 +0530

    a,b=map(int,input().split()) gcd=0 for i in range(1,a+1):     if a%i==0 and b%i==0:         gcd=i print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,6 +1,6 @@
 a,b=map(int,input().split())
-lst=[]
-for i in range(1,max(a,b)):
+gcd=0
+for i in range(1,a+1):
     if a%i==0 and b%i==0:
-        lst.append(i)
-print(max(lst))
+        gcd=i
+print(gcd)

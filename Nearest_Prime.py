a9f7210e3f587dc30e8f27294248e8e12c45e082
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2024-03-10 22:40:23 +0530

    import math def prime(n):     if n<2:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True for _ in range(int(input())):     n=int(input())     t1=n     t2=n     while not prime(t1):         t1-=1     while not prime(t2):         t2+=1     if(n-t1>t2-n):         print(t2)     else:         print(t1)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,24 +1,21 @@
 import math
-def is_prime(n):
-    if(n<2):
+def prime(n):
+    if n<2:
         return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+for _ in range(int(input())):
+    n=int(input())
+    t1=n
+    t2=n
+    while not prime(t1):
+        t1-=1
+    while not prime(t2):
+        t2+=1
+    if(n-t1>t2-n):
+        print(t2)
     else:
-        for i  in range(2,int(math.sqrt(n))+1):
-            if(n%i==0):
-                return False
-        return True
-n=int(input())
-for k in range(n):
-    p=int(input())
-    if(is_prime(p)):
-        print(p)
-    else:
-        np,bp=p+1,p-1
-        while(not is_prime(np)):
-            np+=1
-        while(not is_prime(bp)):
-            bp-=1
-        if(np-p<p-bp):
-            print(np)
-        else:
-            print(bp)
+        print(t1)
+        

7246e4670d82e25364a81db6c790fc578bd9d5d4
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2024-03-20 21:38:56 +0530

    import math def prime(n):     if n<2:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True
    n=int(input())
    s,sq=0,0
    for i in str(n):
        s+=int(i)
        sq+=int(i)**2
    if(prime(sq-s)):
        print("Prime")
    else:
        print("Not Prime")

--- Prime.py
+++ Prime.py
@@ -1,9 +1,19 @@
+import math
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+    
+    
 n=int(input())
-s=0
-for i in range(1,n+1):
-    if n%i==0:
-        s+=1
-if s==2:
+s,sq=0,0
+for i in str(n):
+    s+=int(i)
+    sq+=int(i)**2
+if(prime(sq-s)):
     print("Prime")
 else:
     print("Not Prime")
